ae20701e3dcf6104d21ea0e0588ea0b4b30e334e
rholette5816 <kevinrholette@gmail.com>
2026-04-13 15:10:09 +0800

    Redirect hinilas-pro.vercel.app to hinilas.pro

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,16 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com"],
   },
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "hinilas-pro.vercel.app" }],
+        destination: "https://hinilas.pro/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
